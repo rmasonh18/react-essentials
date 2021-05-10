87be2cb6407b8207877d515e622f90bb5ebe5ae9
Robert Hendricks <rh12@email.sc.edu>
2021-05-10 11:09:47 -0400

    create header component~simple

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import logo from './logo.svg';
+//.import logo from './logo.svg';
 import './App.css';
+
+
+
+// create header component
+function Header() {
+  return (
+    <header>
+      <h1>Jackie's Kitchen</h1>
+    </header>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-    <h1>Header</h1>
+    <Header />
     <h2>Main</h2>
     <h3>Footer</h3>
     </div>
