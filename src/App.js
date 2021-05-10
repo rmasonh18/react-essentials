2ae0dd48435517e8094dfafdf347512dacc799a6
Robert Hendricks <rh12@email.sc.edu>
2021-05-10 12:52:16 -0400

    add image to site using image name instead url in img src

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 //.import logo from './logo.svg';
 import './App.css';
+import jackie from "./profile.png";
 
 
 
 // create header component
 function Header(props) {
   //add console to check in browserslist
-  console.log(props);
+  //console.log(props);
   return (
     <header>
       <h1>{props.name}'s Kitchen</h1>
@@ -19,8 +20,9 @@ function Main(props) {
   return (
     <section>
       <p>We serve the most {props.adjective} food around.</p>
+      <img src = {jackie} height ={200} alt ="Jackie"/>
       <ul style = {{textAlign: 'left'}}>
-      {props.dishes.map((dish) => <li>{dish}</li>)}
+      {props.dishes.map((dish) => <li key = {dish.id}>{dish.title}</li>)}
 
       </ul>
     </section>
@@ -40,8 +42,13 @@ function Footer(props) {
 const dishes = [
   "macaroni and cheese",
   "Salmon",
-  "Tofu and vegetables"
+  "Tofu and vegetables",
+  "New York Strip",
+  "Tenderloin"
 ];
+
+//create dishobject with id and title
+const dishObjects = dishes.map((dish, i) => ({id: i,  title: dish }));
 
 
 
@@ -50,7 +57,7 @@ function App() {
   return (
     <div className="App">
     <Header name="Jackie" />
-    <Main adjective="amazing" dishes = {dishes}/>
+    <Main adjective="amazing" dishes = {dishObjects}/>
     <Footer year={new Date().getFullYear()}/>
     </div>
   );
