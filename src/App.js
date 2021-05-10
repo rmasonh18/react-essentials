e7a1a3c332608ace4ee150ae6a47ae12cc314cf7
Robert Hendricks <rh12@email.sc.edu>
2021-05-10 17:08:03 -0400

    adding more useEffect

--- src/App.js
+++ src/App.js
@@ -8,17 +8,27 @@ import './App.css';
 function App() {
  // use shorthand react.fragment for conditional rendering terinary statement
  const [emotion, setEmotion] = useState("happy");
+ const [secondary, setSecondary] = useState("Tired");
+
+
  console.log(emotion);
 
  //useEffect
  useEffect(() => {
    console.log(`It is ${emotion} around here`);
  }, [emotion]);
+ useEffect(() => {
+   console.log(`It is ${secondary} around here`);
+ } ,[secondary]);
  return (
   <>
-    <h1>Current emotion is { emotion } </h1>
+    <h1>Current emotion is { emotion } not { secondary} </h1>
+    <h1>guess</h1>
     <button onClick={() => setEmotion("HAPPY")}>Be Happy</button>
     <button onClick = {() => setEmotion("Frustrated")}>Change Emotion</button>
+    <button onClick = {() => setSecondary("CRABBY")}>Make Crabby</button>
+   
+   
     <button onClick = {() => setEmotion("Enthusatic")}>Enthuse</button>
   </>
  );
