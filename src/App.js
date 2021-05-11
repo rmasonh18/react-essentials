2fb792d230b9518e58fcf49e13f48002557f8c15
Robert Hendricks <rh12@email.sc.edu>
2021-05-10 21:33:19 -0400

    tests using function and test ~ library

--- src/App.js
+++ src/App.js
@@ -6,40 +6,17 @@ import './App.css';
 //https://api.github.com/users/rmasonh18
 
  
- function App({ login }) {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-  useEffect(() => {
-    if (!login) return
-    setLoading(true);
-    console.log(loading)
-    fetch(`https://api.github.com/users/${login}`)
-      .then((response) => response.json())
-      .then(setData)
-      .then(() => setLoading(false))
-      .catch(setError);
-  }, [login]);
-  if (loading) return <h1> LOADING .....</h1>
-  if (error) return
-  <pre>{JSON.stringify(error, null, 2)}</pre>
-  if (!data) return null;
-  if (data) {
-    return (
-      <div>
-        <h1>NAME: <strong></strong>{data.name}</h1>
-        <p>{data.blog}</p>
-     
-        <p>PUBLIC REPOS: {data.public_repos}</p>
-        <img alt ={data.login} src={data.avatar_url} />
-      </div>
+ function App() {
+    // test react 
+    return(
+    
+    
+    <div>Hello React Test Library</div>
     );
   }
 
 
-  return <div>No User Available</div>;
-    
-}
+
   
 
 export default App;
---
+++ src/Checkbox.test.js
@@ -0,0 +1,11 @@
+import { fireEvent, render } from "@testing-library/react";
+import React from "react";
+import { Checkbox } from "./Checkbox";
+
+test("Selecting checkbox", () => {
+    //test
+    const { getByLabelText} = render(<Checkbox />);
+    const checkbox = getByLabelText(/not checked/);
+    fireEvent.click(checkbox);
+    expect(checkbox.checked).toEqual(true);
+});
